64267182f88acd74f23a3fdbb1329df66b47bb9e
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-02-21 18:36:00 -0300

    Added a JS backend for post admin.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const bodyParser = require('body-parser');
 const path = require('path');
-const fs = require('fs');
 const hbs = require('hbs');
 
 const express = require('express');
@@ -10,35 +9,62 @@ const PORT = process.env.PORT || 5000;
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'partials'));
 
-app.use((req, res, next) => {
-    let now = new Date().toString();
-    let log = `${now}: ${req.method}`;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
 
-    console.log(log);
-    fs.appendFile('server.log', `${log} :: ${req.url}\n`, (err) => {
-        if (err) {
-            console.err('Unable to append to server.log');
-        }
-    });
-
-    next();
-});
-
-const users = [
-    {name: 'Tiz'},
-    {name: 'Trombeta'},
-    {name: 'Coquinho'},
-];
-
-app.get('/users', (req, res) => {
-    res.json(users);
-});
+const posts = require('./posts');
 
 app.get('/', (req, res) => {
-    res.render('help.hbs', users);
+    res.render('index.hbs');
 });
 
+app.get('/help', (req, res) => {
+    const user = users[0];
+
+    res.render('help.hbs', {
+        user,
+        currentYear: new Date().getFullYear(),
+    });
+});
+
+app.get('/api/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.get('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id);
+
+    if (!post) {
+        res.status(404).send(`Post with id ${id} does not exist.`);
+    }
+
+    res.send(post)
+});
+
+app.put('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id);
+    
+    post.title = req.body.title;
+    post.body = req.body.body;
+
+    res.send(posts);
+});
+
+app.delete('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id);
+
+    const index = posts.indexOf(post);
+    posts.splice(index, 1);
+
+    res.send(posts);
+});
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
