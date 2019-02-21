ad7223c3f6b9023f32d1cf58b202affbc0e6f234
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-02-21 17:24:20 -0300

    Using hbs render engine.

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const hbs = require('hbs');
 
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method}`;
+
+    console.log(log);
+    fs.appendFile('server.log', `${log} :: ${req.url}\n`, (err) => {
+        if (err) {
+            console.err('Unable to append to server.log');
+        }
+    });
+
+    next();
 });
+
+const users = [
+    {name: 'Tiz'},
+    {name: 'Trombeta'},
+    {name: 'Coquinho'},
+];
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.get('/', (req, res) => {
+    res.render('help.hbs', users);
+});
+
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
